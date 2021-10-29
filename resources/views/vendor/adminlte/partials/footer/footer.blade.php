639ff06479e876e784e09f7ce24bdf725a03e835
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2021-10-29 10:20:41 +0700

    footer

--- resources/views/vendor/adminlte/partials/footer/footer.blade.php
+++ resources/views/vendor/adminlte/partials/footer/footer.blade.php
@@ -1,3 +1,14 @@
 <footer class="main-footer">
-    @yield('footer')
+    © Copyright <strong><a href="{{route('dashboard')}}">Koperasi SMK 5 Kuningan </a> {{ Illuminate\Support\Carbon::createFromFormat('Y-m-d H:i:s', now())->year }}.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> on Developer
+    </div>
 </footer>
+
+{{-- <footer class="main-footer">
+    @yield('footer')
+
+
+</footer> --}}
+
